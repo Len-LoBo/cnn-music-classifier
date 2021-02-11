0bdbb52e8976126d8a18ab101a7b8547136fba46
Leonard LoBiondo <llobiondo2@gmail.com>
2021-02-11 12:38:14 -0800

    changed port info in app.py

--- web/app.py
+++ web/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 import tensorflow as tf
 import tensorflow.keras as keras
@@ -73,4 +74,5 @@ def predict(model, X):
     return prediction
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
